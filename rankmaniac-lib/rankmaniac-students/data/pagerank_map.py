aa79be803f865e26a87be8eed78024dbd3ae6ec7
Manasa <mashok@caltech.edu>
2016-02-07 09:44:38 -0800

    Started map function
    Added code to distribute page rank to neighbors of each node

--- rankmaniac-lib/rankmaniac-students/data/pagerank_map.py
+++ rankmaniac-lib/rankmaniac-students/data/pagerank_map.py
@@ -7,4 +7,13 @@ import sys
 #
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    #sys.stdout.write(line)
+    values = line.split("\t")
+    beginning = values[0].split(":")
+    node_id = int(beginning[1])
+    node_info = map(float, values[1].rstrip("\n").split(","))
+    neighbors = node_info[2:-1]
+    curr_rank = node_info[0]
+    num_neighbors = len(neighbors)
+    for n in neighbors:
+        sys.stdout.write("%d %f" % (n, curr_rank / num_neighbors))
