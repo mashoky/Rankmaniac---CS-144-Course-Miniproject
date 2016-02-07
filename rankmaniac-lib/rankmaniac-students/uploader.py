8e064c0e03fafe4e3987680f1b0b263e0a48b3b0
Manasa <mashok@caltech.edu>
2016-02-07 13:33:38 -0800

    Made amazon credentials private

--- rankmaniac-lib/rankmaniac-students/uploader.py
+++ rankmaniac-lib/rankmaniac-students/uploader.py
@@ -98,8 +98,8 @@ def do_main(team_id, access_key, secret_key,
 
 if __name__ == '__main__':
 
-    team_id = 'xtreme'
-    access_key = 'AKIAJIFYAEFRJVR5KR3A'
-    secret_key = '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'
+    team_id = 'id'
+    access_key = 'key'
+    secret_key = 'secret'
 
     do_main(team_id, access_key, secret_key)
