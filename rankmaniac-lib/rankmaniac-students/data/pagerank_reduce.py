7957b4be062e6c955fc7e61fd60dd60f1fa4729f
Manasa <mashok@caltech.edu>
2016-02-08 09:21:37 -0800

    Fixing map function
    Make it work for later iterations

--- rankmaniac-lib/rankmaniac-students/data/pagerank_reduce.py
+++ rankmaniac-lib/rankmaniac-students/data/pagerank_reduce.py
@@ -6,14 +6,17 @@ import sys
 # This program simply represents the identity function.
 #
 sum = 0
-node_idx = 0
+prev_idx = -1
 for line in sys.stdin:
     sys.stdout.write(line)
     values = line.split("\t")
     node_idx = int(values[0])
-    
-    sum += float(values[1])
-new_rank = (0.85 * sum) + 0.15
-sys.stdout.write("FinalRank:%f\t%s" % (float(new_rank), str(node_idx)))
-#    
+    if node_idx == prev_idx or prev_idx == -1:
+        sum += float(values[1])
+    else:      
+        new_rank = (0.85 * sum) + 0.15
+        sys.stdout.write("FinalRank:%f\t%s" % (float(new_rank), str(prev_idx)))
+        sum = 0
+        sum += float(values[1])
+    prev_idx = node_idx
 
