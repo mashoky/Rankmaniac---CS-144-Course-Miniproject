b02fa01380350cb32fa84ebdb7c8e8fc6e473140
Manasa <mashok@caltech.edu>
2016-02-08 15:49:49 -0800

    Fixed issues with output
    Ran 2 iterations

--- rankmaniac-lib/rankmaniac-students/data/pagerank_map.py
+++ rankmaniac-lib/rankmaniac-students/data/pagerank_map.py
@@ -10,18 +10,29 @@ data = {}
 for line in sys.stdin:
     #sys.stdout.write(line)
     if line.startswith('LATER_ITER'):
-        pass 
+        values = line.split("\t")
+        node_id = int(values[1])
+        node_info = values[2].rstrip("\n").split( )
+        if len(node_info) < 3:
+            neighbors = []
+        else:
+            neighbors = map(int, node_info[2].split(","))
+        curr_rank = float(node_info[0])
+        prev_rank = float(node_info[1])
+        num_neighbors = len(neighbors)
+        data[node_id] = (curr_rank,prev_rank, neighbors)
             
     else:
         values = line.split("\t")
         beginning = values[0].split(":")
         node_id = int(beginning[1])
         node_info = values[1].rstrip("\n").split(",")
-        neighbors = map(int, node_info[2:-1])
+        neighbors = map(int, node_info[2:])
         curr_rank = float(node_info[0])
         prev_rank = float(node_info[1])
         num_neighbors = len(neighbors)
         data[node_id] = (curr_rank,prev_rank, neighbors)
+
 for node in data.itervalues():
     for n in node[2]:
         p_rank = data[n][1]
@@ -29,5 +40,5 @@ for node in data.itervalues():
         curr_rank = node[0]
         num_neighbors = len(node[2])
         sys.stdout.write("%d\t%f %f %s\n" % (n, curr_rank / num_neighbors, \
-        p_rank, ','.join(str(id) for id in n_neighbors) ))        
-        
+        curr_rank, ','.join(str(id) for id in n_neighbors) ))     
+
--- rankmaniac-lib/rankmaniac-students/data/pagerank_reduce.py
+++ rankmaniac-lib/rankmaniac-students/data/pagerank_reduce.py
@@ -10,29 +10,35 @@ prev_idx = -1
 changed = False
 print_buffer = []
 for line in sys.stdin:
-    sys.stdout.write(line)
+    #sys.stdout.write('Line:%s' %line)
     values = line.split("\t")
     node_idx = int(values[0])
-    node_info = values[1].split( )
-    #print len(node_info)
+    node_info = values[1].rstrip("\n").split( )
     if node_idx == prev_idx or prev_idx == -1:
-        #print values[1]
         sum += float(node_info[0])
         prev_rank = float(node_info[1])
-        prev_info = node_info[2]
+        if(len(node_info) < 3):
+            prev_info = ''
+        else:
+            prev_info = node_info[2]
+
     else:      
         new_rank = (0.85 * sum) + 0.15
-        if new_rank != prev_rank:
-            changed = True
-        if changed:
-            #sys.stdout.write("%d\t%f %f %s" % (prev_idx, new_rank, prev_rank, prev_info))
-            pass
-        else:
-            print_buffer.append("%d\t%f %f %s" % (prev_idx, new_rank, prev_rank, prev_info))
+        #print 'Change' + str(changed)
+        #print prev_info
+        sys.stdout.write("LATER_ITER\t%d\t%f %f %s\n" % (prev_idx, new_rank, prev_rank, prev_info))
+        #else:
+        #    print_buffer.append("%d\t%f %f %s" % (prev_idx, new_rank, prev_rank, prev_info))
         #sys.stdout.write("FinalRank:%f\t%s" % (float(new_rank), str(prev_idx)))
         #sys.stdout.write("%d\t%f %f %s" % (prev_idx, new_rank, prev_rank, prev_info))
         
         sum = 0
-        sum += float(values[1])
+        sum += float(node_info[0])
+        prev_rank = float(node_info[1])
+        if(len(node_info) < 3):
+            prev_info = ''
+        else:
+            prev_info = node_info[2]
     prev_idx = node_idx
-
+new_rank = (0.85 * sum) + 0.15
+sys.stdout.write("LATER_ITER\t%d\t%f %f %s\n" % (prev_idx, new_rank, prev_rank, prev_info))
